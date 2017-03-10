1818055a64c2826c1d974dbd3e7543d86f22dd0b
bysuggestion <609273175@qq.com>
2017-03-10 16:29:23 +0800

    First Commit

---
+++ app/src/main/java/com/example/wang/edrive/Config.java
@@ -0,0 +1,10 @@
+package com.example.wang.edrive;
+
+/**
+ * Created by wang on 2017-3-10.
+ */
+
+public class Config {
+    public final static String KEY_TOKEN=null;
+    public static final String APP_ID="com.example.wang.edrive";
+}
---
+++ app/src/main/java/com/example/wang/edrive/netService/netConnection.java
@@ -0,0 +1,8 @@
+package com.example.wang.edrive.netService;
+
+/**
+ * Created by wang on 2017-3-10.
+ */
+
+public class netConnection {
+}
